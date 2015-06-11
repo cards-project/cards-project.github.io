ffa615d5de0b87dbf45fb348daaf965575d5382a
Michael Jennings <mpjngs@gmail.com>
2015-06-11 15:23:59 -0700

    links

--- src/components/links.js
+++ src/components/links.js
@@ -1,36 +1,61 @@
-
+var Selector = require('/home/mjennings/pagebuilder/selector.js')
 var sty = require('../styles/style.js')
 var stys = require('../styles/styles.js')
 var el = require('../el.js')
 
 module.exports = function(){
-  function item(){
-    return el('div').style('width', '5em')
-    .content(
-      el('div').style(
-        sty('position', 'absolute')
+
+  function baselineText(text, size, dir){
+    return el('div').style(
+      sty('height', size)
+    ).content(
+      el('span').style(
+        sty('line-height', '0'),
+        sty('font-size', size),
+        (dir === 'top' ? sty('vertical-align', 'text-top') : {})
       ).content(
-        el('div').style(
-          sty('font-size', '2rem'),
-          sty('height', '3rem'),
-          sty('position', 'relative'),
-          stys.flex('row', 'center', 'flex-end')
-        ).content(
-          el('span').style(
-            stys.collapseLine('bottom'),
-            sty('overflow', 'hidden')
-          ).content(
-            'test'
-          )
-        ),
-        el('div').style(
-          sty('font-size', '1.3rem'),
-          stys.collapseLine('top'),
-          sty('text-align', 'center')
-        ).content(
-          'subtitle'
-        )
+        '&nbsp;' + text
       ),
+      el('span').style(
+        sty('display', 'inline-block'),
+        sty('height', size),
+        sty('content', '""')
+      )
+    )
+  }
+
+  function text(){
+
+    var title = el('div').style(
+      sty('font-size', '2rem'),
+      sty('height', '3rem'),
+      sty('position', 'relative'),
+      sty('text-align', 'center')
+    ).content(
+      baselineText('TEST', '3rem')
+    )
+
+    var subtitle = el('div').style(
+      sty('font-size', '.87rem'),
+      sty('height', '1.3rem'),
+      sty('position', 'relative'),
+      sty('text-align', 'center')
+    ).content(
+      baselineText("<i>SUBTITLE</i>", '1.3rem', 'top')
+    )
+
+    return el('div').style(
+      sty('position', 'absolute')
+    ).content(
+      title,
+      subtitle
+    )
+  }
+
+  function item(){
+    return el('div').style(stys.dims('10em', '3rem'))
+    .content(
+      text(),
       el('div').style(
         sty('background', 'grey'),
         stys.dims('100%', '3rem'),
